7e8cce54b90cc37c3bb5c0aebbd001c444b0841c
JoFrhwld <JoFrhwld@gmail.com>
2023-12-07 12:50:29 -0500

    big progress!

--- _extensions/codeblocklabel/codeblocklabel.lua
+++ _extensions/codeblocklabel/codeblocklabel.lua
@@ -1,26 +1,17 @@
-local function makeCodeName(codename)
-  local codediv = pandoc.Div(
-    pandoc.Span(codename, {class = "langname"}),
-    {class = "langname"}
-  )
-  return codediv
-end
-
-function Div(div)
-  local out = nil
-  -- out = tostring(div.classes[1])
-  if div.classes:includes("cell") then
-      local content = div.content
-      table.insert(content, 1, makeCodeName(div.content[1].classes[1]))
-      out = pandoc.Div(
-        content
-      )
-      out.attr = div.attr
-      out.identifier = div.identifier
-      --return out
-  elseif div.classes:includes("sourceCode") then
-    out = tostring(div.classes[1])
+function CodeBlock(block)
+  local newblock = block
+  if (FORMAT:match "html") and 
+     (block.classes[1]) then
+    --newblock.text = tostring(block.classes[1]).."\n"..block.text
+    local langname = block.classes[1]
+    out = {pandoc.Div(
+      pandoc.Span(block.classes[1], {class = "langnametext"}),
+      {class = "langname"}
+    ), 
+      newblock
+    }
+     else
+      out = newblock
   end
   return out
 end
-
